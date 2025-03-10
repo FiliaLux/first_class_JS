436c4a6db90b99beaa22a298f3a9d601545ef78f
Angie <rodriguezangela95@gmail.com>
2025-03-10 21:54:33 +0100

    Arrays practice

--- examples/05_arrays.js
+++ examples/05_arrays.js
@@ -7,6 +7,7 @@ products.push("Pants")
 productPrices.pop()
 const capIndex = products.indexOf("Cap");
 products.splice(capIndex, 1)
+
 const csvItems = "PC,PS5,XBox,iPhone"
 const parseCsvItems = (line) => {
     const csvProducts = csvItems.split(",")
@@ -18,3 +19,37 @@ const parseCsvItems = (line) => {
 }
 
 console.log(products.reverse(), productPrices.indexOf(8),parseCsvItems(csvItems))
+
+let numbers = [];
+numbers = ("1,2,3,4,5".split(","))
+console.log(numbers)
+numbers.splice(3,0,"keepcoding")
+let msg = ""
+if (numbers.includes("keepcoding")) console.log("This array doesn't only contain numbers");  
+numbers.unshift(10)
+console.log(numbers)
+console.log(numbers.indexOf("keepcoding"))
+numbers.splice(4,1)
+console.log(numbers)
+console.log(numbers.length)
+
+/*  Los tres puntos dentro de un nuevo array nos
+permite coger a otro como referencia sin mo-
+dificar el nuevo array cuando el de referencia 
+cambia. */
+const productUser1 = ["TV", "Laptop", "PS5"];
+const productUser2 = [...productUser1]; 
+
+const productList1 = productUser1.push("Cap");
+console.log(productUser1,productUser2)
+
+const addProduct = (newProduct,productList) => {
+    const newProductList = [...productList]
+    newProductList.push(newProduct)
+    return newProductList
+};
+
+console.log(addProduct("Socks",productUser2))
+
+
+
